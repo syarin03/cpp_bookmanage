fe72e48fa3865c4227c72b18346f8b389deca01a
syarin03 <gami5507@gmail.com>
2022-07-01 15:52:46 +0900

    Initial commit

---
+++ book.h
@@ -0,0 +1,14 @@
+#ifndef BOOKMANAGE_BOOK_H
+#define BOOKMANAGE_BOOK_H
+
+
+class Book {
+private:
+    int num;
+    char * name;
+    char * writer;
+    bool isrent;
+};
+
+
+#endif //BOOKMANAGE_BOOK_H
---
+++ user.h
@@ -0,0 +1,19 @@
+#ifndef BOOKMANAGE_USER_H
+#define BOOKMANAGE_USER_H
+
+
+class User {
+private:
+    char * id;
+    char * pw;
+    char * name;
+    char phone[11];
+public:
+    User() {
+        // empty
+    }
+    void setID
+};
+
+
+#endif //BOOKMANAGE_USER_H
